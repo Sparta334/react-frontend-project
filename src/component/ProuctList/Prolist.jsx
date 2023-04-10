e2f31893d84f3b272f5f50ed4864620a451400be
Spartan-117 <ianmina0322@gmail.com>
2023-04-10 23:33:28 +0800

    sss

--- src/component/ProuctList/Prolist.jsx
+++ src/component/ProuctList/Prolist.jsx
@@ -76,7 +76,7 @@ export default function Prolist({Title , InputJson  }){
                 {       
 
 
-                        InputJson.data.recomms.map((product ,index)=> (
+                        InputJson.data.map((product ,index)=> (
                             <Space size={32}>
                             <div className="ImageBox" key={index} >
 
